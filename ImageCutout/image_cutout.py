ddb61e5d0ab13327bf17d749506230de34ce12e6
Yichen <yichencharles.cai@zebra.com>
2023-02-06 16:52:19 -0500

    Bug fix

--- ImageCutout/image_cutout.py
+++ ImageCutout/image_cutout.py
@@ -26,18 +26,20 @@ def main(path, out_path, mask_size_multiplier):
         out_path = path
     count = 0
     with tqdm(total=100) as pbar:
-        for filename in sorted(os.listdir(path)):
+        for filename in os.listdir(path):
             f_path = os.path.join(args.img_path, filename)
             img = cv2.imread(f_path)
+            if img is None:
+                continue
             height, width, c = img.shape
-            mask_size = (min(height, width)) * mask_size_multiplier
+            mask_size = int((min(height, width)) * mask_size_multiplier)
             h_mask = random.randint(0, height-mask_size-1)
             w_mask = random.randint(0, width-mask_size-1)
             new_img = apply_mask(img, h_mask, w_mask, mask_size)
             save_path = os.path.join(out_path, filename[:-4] + "_cutout.jpg")
             cv2.imwrite(save_path, new_img)
             count += 1
-            if count % 1065 == 0:
+            if count % 1165 == 0:
                 pbar.update(1)
 
 
